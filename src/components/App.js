136e07184bb290353c37a8c0bbe84b821c42c25d
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-10-31 01:00:50 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,43 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
+let d = new Date();
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      string: `${
+        d.getHours() % 12
+      }+":"+${d.getMinutes()}+":"+${d.getSeconds()}+" "+${
+        d.getHours() >= 12 ? "PM" : "AM"
+      }`
+    };
+  }
+  componentDidMount() {
+    const k = setInterval(
+      (d = new Date()),
+      this.setState(() => {
+        this.state.string = `${
+          d.getHours() % 12
+        }+":"+${d.getMinutes()}+":"+${d.getSeconds()}+" "+${
+          d.getHours() >= 12 ? "PM" : "AM"
+        }`;
+      }),
+      1000
+    );
+    return () => {
+      clearInterval(k);
+    };
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time" className="Date">
+          {this.state.string}
+        </h3>
+      </div>
+    );
+  }
 }
-
 
 export default App;
